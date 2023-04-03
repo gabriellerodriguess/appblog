3357f404c60a8b518bc7a578a441fadb0d3cb440
Gabrielle Rodrigues <gabriellerut@gmail.com>
2023-04-03 18:18:34 -0300

    feat(004): create home page and post component

--- config/app.js
+++ config/app.js
@@ -6,6 +6,8 @@ import path from 'path'
 import mongoose from "mongoose"
 import session from 'express-session'
 import flash from 'connect-flash'
+import modelPosts from "../models/Post.js"
+import modelCategory from "../models/Category.js"
 
 const app = express()
 const hbs = create({})
@@ -55,6 +57,55 @@ app.use((req,res,next) => {
     res.locals.error_msg = req.flash('error_msg')
 
     next()
+})
+
+app.get('/', (req,res) => {
+    modelPosts.find().lean().populate('category').sort({date:'desc'}).then((posts) => {
+        res.render('index', ({posts: posts}))
+    }).catch((req,res) => {
+       console.log('error_msg', 'Ocurred a intern error. Try again.')
+       res.redirect('/404')
+    })
+})
+
+app.get('/posts/:slug', (req,res) => {
+    modelPosts.findOne({slug: req.params.slug}).lean().then((post) => {
+        if(post) {
+            console.log(post)
+            res.render('post/index', ({post: post}))
+        } else {
+            res.redirect('/')
+        }
+    }).catch((error => {
+        console.log(error)
+        res.redirect('/')
+    }))
+})
+
+app.get('/categories', (req,res) => {
+    modelCategory.find().lean().then((categories) => {
+        console.log(categories)
+        res.render('categories/index', ({categories: categories}))
+    }).catch((error) => {
+        console.log(`An error occurred. ${error}`)
+    })
+})
+
+app.get('/categories/:slug', (req, res) => {
+    modelCategory.findOne({slug: req.params.slug}).lean().then((category) => {
+        if(category) {
+            modelPosts.find({category: category._id}).populate('category').lean().then((posts) => {
+                res.render('categories/posts', ({posts: posts, category: category}))
+            }).catch((error) => {
+                console.log(`An error occurred when category listed. ${error}`)
+                res.redirect('/')
+            })
+        }
+    })
+})
+
+app.get('/404', (req,res) => {
+    res.send('Ocurred a intern error. Try again.')
 })
 
 app.listen(port, () => {
